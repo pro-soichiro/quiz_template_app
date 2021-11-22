ffd72d697273bfceb516cd93069261921b8c34da
pro-soichiro <dr.1987626@gmail.com>
2021-11-22 11:53:02 +0000

    [Update] スタッフのマイページ　実装途中 chart.js以外

--- app/controllers/public/staffs_controller.rb
+++ app/controllers/public/staffs_controller.rb
@@ -1,10 +1,32 @@
 class Public::StaffsController < ApplicationController
-  def show
-  end
+  # before_action :authenticate_public!
+  before_action :ensure_staff, only: [:show,:update]
 
   def index
   end
 
+  def show
+  end
+  
   def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def ensure_staff
+    @staff = Staff.find(params[:id])
+  end
+
+  def staff_params
+    params.require(:staff).permit(:last_name,
+                                  :last_name_kana,
+                                  :first_name,
+                                  :first_name_kana,
+                                  :email,
+                                  :employ_number,
+                                  :image)
   end
 end
